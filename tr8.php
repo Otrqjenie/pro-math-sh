202efd7aa76cf1e30c9c177364c97f3895959058
Otrajenie <kolhoznaya43@mail.ru>
2017-12-13 04:48:38 +0700

    ok

--- tr8.php
+++ tr8.php
@@ -26,73 +26,88 @@ header("Content-Type: text/html; charset=utf-8");
 
 
 
-function Fun($dir)
-{
-	// $d = dir($dir);
-	// $v = 0;
-	// $d->handle;
-	// $d->path;
-	// while (false !== ($entry = $d->read())) {
-	//    	if ($dir !== ".") {
-	//    		$str = "hello";
-	//    		global $str;
-	//    		$f_name = $dir."/".$entry;
-	//    		$v = $v + filesize($f_name);
-	//    		// echo $f_name."-".filesize($f_name)."<br>";
-	//    	}
-	//    	else{
-	//    		$f_name = $entry;
-	//    		$v = $v + filesize($f_name);
-	//    		// echo $f_name."-".filesize($f_name)."<br>";
-	//    	}
-	// };
-	global $v;
-	$v = filesize($dir);
-	$v = filter_var($v, FILTER_VALIDATE_INT);
-	return $v;
-	// $d->close();
+// function Fun($dir)
+// {
+// 	// $d = dir($dir);
+// 	// $v = 0;
+// 	// $d->handle;
+// 	// $d->path;
+// 	// while (false !== ($entry = $d->read())) {
+// 	//    	if ($dir !== ".") {
+// 	//    		$str = "hello";
+// 	//    		global $str;
+// 	//    		$f_name = $dir."/".$entry;
+// 	//    		$v = $v + filesize($f_name);
+// 	//    		// echo $f_name."-".filesize($f_name)."<br>";
+// 	//    	}
+// 	//    	else{
+// 	//    		$f_name = $entry;
+// 	//    		$v = $v + filesize($f_name);
+// 	//    		// echo $f_name."-".filesize($f_name)."<br>";
+// 	//    	}
+// 	// };
+// 	global $v;
+// 	$v = filesize($dir);
+// 	$v = filter_var($v, FILTER_VALIDATE_INT);
+// 	return $v;
+// 	// $d->close();
 	
+// };
+
+// function vol()
+// {
+// 	$volium = 0;
+// 	$a = array();
+// 	// $a[] = 'arena.php';
+// 	$a[] = 'css/arena.css';
+// 	$a[] = 'css/game_js.css';
+// 	$a[] = 'css/main.css';
+// 	$a[] = 'css/tr_js.css';
+// 	$a[] = 'arena.php';
+// 	$a[] = 'game_js.php';
+// 	$a[] = 'game_js_invitation.php';
+// 	$a[] = 'game_js_load.php';
+// 	$a[] = 'game_js_online.php';
+// 	$a[] = 'game_js_register.php';
+// 	$a[] = 'game_js_rez.php';
+// 	$a[] = 'index.php';
+// 	$a[] = 'js.php';
+// 	$a[] = 'js_fon.php';
+// 	$a[] = 'lis.php';
+// 	$a[] = 'tr_js.php';
+// 	$a[] = 'tr8.php';
+// 	$a[] = 'lib/arena.js';
+// 	$a[] = 'lib/connect.php';
+// 	$a[] = 'lib/lib_js.php';
+// 	$a[] = 'lib/main.js';
+// 	$a[] = 'lib/mainn.js';
+// 	$a[] = 'lib/tr_js.js';
+// 	foreach ($a as $key => $value) {	
+// 		$volium = $volium + Fun($value);
+// 		echo $value."-".Fun($value)."<br>";
+// 	};
+// 	return $volium;
+// };
+// $r = vol();
+// print_r($r);
+// echo "<pre>";
+// print_r($str);
+// echo "</pre>";
+
+
+// Игра
+
+for ($i=0; $i < 10 ; $i++) { 
+	print_r($i);
 };
 
-function vol()
-{
-	$volium = 0;
-	$a = array();
-	// $a[] = 'arena.php';
-	$a[] = 'css/arena.css';
-	$a[] = 'css/game_js.css';
-	$a[] = 'css/main.css';
-	$a[] = 'css/tr_js.css';
-	$a[] = 'arena.php';
-	$a[] = 'game_js.php';
-	$a[] = 'game_js_invitation.php';
-	$a[] = 'game_js_load.php';
-	$a[] = 'game_js_online.php';
-	$a[] = 'game_js_register.php';
-	$a[] = 'game_js_rez.php';
-	$a[] = 'index.php';
-	$a[] = 'js.php';
-	$a[] = 'js_fon.php';
-	$a[] = 'lis.php';
-	$a[] = 'tr_js.php';
-	$a[] = 'tr8.php';
-	$a[] = 'lib/arena.js';
-	$a[] = 'lib/connect.php';
-	$a[] = 'lib/lib_js.php';
-	$a[] = 'lib/main.js';
-	$a[] = 'lib/mainn.js';
-	$a[] = 'lib/tr_js.js';
-	foreach ($a as $key => $value) {	
-		$volium = $volium + Fun($value);
-		echo $value."-".Fun($value)."<br>";
-	};
-	return $volium;
-};
-$r = vol();
-print_r($r);
-echo "<pre>";
-print_r($str);
-echo "</pre>";
+
+
+
+
+
+// ----------------------------------------
+
 
 ?>
 <form action="<?=$_SERVER['SCRIPT_NAME']?>"  method = "POST">
